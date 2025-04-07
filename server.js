193e52bc6978f8b2f30be8b48627ce71d0c3d664
patrick-codes <khofilyndon2018@gmail.com>
2025-04-07 21:46:24 +0000

    minor fixes cors

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const appRouter = require("./routes/applicationRoutes");
 //const userRouter = require("./routes/usersRoutes");
 const { dbConn } = require("./db_config/dbConnection");
 const dotenv = require("dotenv").config();
-
-
 const cors = require('cors');
-app.use(cors());
+
+
 
 //calling database connection
 dbConn();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/events", appRouter);
 //app.use("/api/events/auth",userRouter);
