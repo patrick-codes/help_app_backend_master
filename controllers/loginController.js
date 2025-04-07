14e755918d23c5d1e677fc309cc6a40447d02745
patrick-codes <khofilyndon2018@gmail.com>
2025-04-07 22:34:43 +0000

    replaced bcrypt with bcryptjs

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,6 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const userDetails = require("../models/userModel");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { request } = require("express");
 
