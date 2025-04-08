c7e8b0813ab56e8dc579fc3e17a4b1ab186785f0
patrick-codes <khofilyndon2018@gmail.com>
2025-04-08 23:27:10 +0000

    signup jwt fix

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -33,66 +33,51 @@ module.exports = {
 
   userSignup: asyncHandler(async (req, res) => {
     const { username, email, password } = req.body;
+  
     if (!username || !email || !password) {
-      res.status(400).json({message:"All fields are required..!!"});
+      res.status(400).json({ message: "All fields are required..!!" });
       throw new Error("All fields are required..!!");
     }
   
     const userAvailable = await userDetails.findOne({ email });
     if (userAvailable) {
-      res.status(401).json({message:"User already Registered"});
+      res.status(401).json({ message: "User already Registered" });
       throw new Error("User already Registered");
     }
   
     const hashedPassword = await bcrypt.hash(password, 10);
+  
     const user = await userDetails.create({
       username,
       email,
       password: hashedPassword,
     });
   
-    //after successful user creation i want to pass the details back to the user
     if (user) {
-      res.status(201).json({ _id: user.id, email: user.email });
+      const accessToken = jwt.sign(
+        {
+          user: {
+            username: user.username,
+            email: user.email,
+            id: user._id,
+          },
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "60m" }
+      );
+  
+      res.status(201).json({
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+        accessToken,
+      });
     } else {
-      res.status(400).json({message:"Details Not Valid"});
+      res.status(400).json({ message: "Details Not Valid" });
       throw new Error("Details Not Valid");
     }
   }),
   
-
- /* userSignup: asyncHandler(async (req, res) => {
-    //const details = await userDetails.find();
-    const { username, email, password } = req.body;
-    if (!username || !email || !password) {
-      res.status(400);
-      throw new Error("All fields are required..!!");
-    }
-
-    const userAvailable = await userDetails.findOne({ email });
-    if (userAvailable) {
-      res.status(400);
-      throw new Error("User already Registered");
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = await userDetails.create({
-      username,
-      email,
-      password: hashedPassword,
-    });
-
-    //after successfull user creation i want to pass the details back to the user
-    if (user) {
-      res.status(201).json({ _id: user.id, email: user.email });
-    } else {
-      res.status(400);
-      throw new Error("Details Not Valid");
-    }
-
-    res.status(200).json(user);
-  }),
-*/
   userfInfo: asyncHandler(async (req, res) => {
     res.status(200).send("current user info..");
   }),
